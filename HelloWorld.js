bf7962eab1dbad9df39136f87abfceccab0da28b
John-Oresh <J.Oresotu@gmail.com>
2023-10-23 17:34:07 +0100

    my first commit

---
+++ HelloWorld.js
@@ -0,0 +1,86 @@
+// Console.log is used to tell Javascript that you want to write a sentence, you type the sentence you want to write inside a bracket after typing console.log
+
+// Example
+
+// console.log("Hello world");
+/*console.log("hello world!!!");
+console.log("how are you!!!");
+console.log("how is today!!!");
+console.log("Today is good!!!");*/
+
+/*console.log("hello world!!!");*/
+//console.log("how are you!!!");
+//console.log("how is today!!!");
+/* console.log("Today is good!!!");
+
+number 1 to infinity;
+String " ";
+Boolean: true or false;
+Array: []
+Object: {} key value */
+
+/* const num = 1;
+const str = "hello here";
+const bol = true;
+const arr = [];
+const obj = {};
+
+console.log(num);
+console.log(bol); */
+
+//const number = '1234567890';
+//const answer = Number(number);
+//console.log(answer)
+
+// const number = '12.43';
+// const answer = parseInt(number);
+// console.log(answer);
+
+/*const number = 35554;
+const answer = number.toString();
+console.log(answer);
+
+const first = 5;
+const second = '5';
+
+if(5 === '5'){
+    console.log('john')
+}else{
+    console.log('samuel')
+}
+
+// const
+// let
+// var*/
+
+/*let age = 10;
+age = 5;
+console.log(age)
+
+function getData () {
+    const name = "John"
+    console.log(name);
+}
+getData ()*/
+
+/*const data = {
+    name:"John",
+    age:5,
+    country:"USA",
+    Hobby:"football",
+    isMale:true,
+    nickname:["smith","frank","rich"]
+}
+
+
+
+console.log(Object.values(data))
+
+const data = (age=10) => {
+    console.log(age.toString());
+
+};
+
+data();*/
+
+// const data = prompt("Enter a word");
